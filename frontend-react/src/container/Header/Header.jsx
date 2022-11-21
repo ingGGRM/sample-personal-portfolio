cbd2d4fca503e923223febd3bed1bf45cb439a41
ing. Genesis G. Rosario <genesis_guarionex_rm@hotmail.com>
2022-11-21 15:43:21 -0400

    started Header development. Added a motion.div element as begining

--- frontend-react/src/container/Header/Header.jsx
+++ frontend-react/src/container/Header/Header.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { motion } from "framer-motion";
 
+import { images } from "../../constants";
 import "./Header.scss";
 
 const Header = () => {
-	return <div>Header</div>;
+	return (
+		<div className="app__header app__flex">
+			<motion.div
+				whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+			></motion.div>
+		</div>
+	);
 };
 
 export default Header;
